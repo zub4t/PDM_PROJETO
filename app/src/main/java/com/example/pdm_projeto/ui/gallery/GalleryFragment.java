46fd007b9c1cfe0dbd3ec45cf836ddb6f51c2acc
pedrompf99 <59067489+pedrompf99@users.noreply.github.com>
2020-03-30 12:44:32 +0100

    teste

--- app/src/main/java/com/example/pdm_projeto/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/pdm_projeto/ui/gallery/GalleryFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,6 +14,7 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.pdm_projeto.MainActivity;
 import com.example.pdm_projeto.R;
 
 public class GalleryFragment extends Fragment {
@@ -20,14 +23,20 @@ public class GalleryFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        galleryViewModel =
-                ViewModelProviders.of(this).get(GalleryViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_gallery, container, false);
-        final TextView textView = root.findViewById(R.id.text_gallery);
+        galleryViewModel = ViewModelProviders.of(this).get(GalleryViewModel.class);
+        final View root = inflater.inflate(R.layout.fragment_gallery, container, false);
+        //final TextView textView = root.findViewById(R.id.text_gallery);
         galleryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+                //textView.setText(s);
+                LinearLayout linearLayout1 = root.findViewById(R.id.linearLayout1);
+                for(int i = 0; i < 20; i++){
+                    ImageView image = new ImageView(getContext());
+                    image.setImageResource(R.drawable.teste2);
+                    linearLayout1.addView(image,472,292);
+                }
+
             }
         });
         return root;
