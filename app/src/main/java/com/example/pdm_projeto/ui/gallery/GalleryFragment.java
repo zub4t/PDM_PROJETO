db4428a7dd8177df532485ad76d6d3104f80f7d5
Marco Augusto <30879430+zub4t@users.noreply.github.com>
2020-03-30 14:01:34 +0100

    resolvendo img

--- app/src/main/java/com/example/pdm_projeto/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/pdm_projeto/ui/gallery/GalleryFragment.java
@@ -1,12 +1,14 @@
 package com.example.pdm_projeto.ui.gallery;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,8 +16,10 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
-import com.example.pdm_projeto.MainActivity;
 import com.example.pdm_projeto.R;
+import com.squareup.picasso.Picasso;
+
+import java.net.URL;
 
 public class GalleryFragment extends Fragment {
 
@@ -33,7 +37,18 @@ public class GalleryFragment extends Fragment {
                 LinearLayout linearLayout1 = root.findViewById(R.id.linearLayout1);
                 for(int i = 0; i < 20; i++){
                     ImageView image = new ImageView(getContext());
-                    image.setImageResource(R.drawable.teste2);
+
+
+                    String imageUri = "http://pdmfcup.ddns.net:8084/PDM/images/City2.jpg";
+
+                   // Picasso.with(getContext()).load(imageUri).into(image);
+
+
+                    Picasso.with(getContext()).load(imageUri)
+                            .placeholder(R.drawable.teste2)
+                            .error(R.drawable.teste);
+
+
                     linearLayout1.addView(image,472,292);
                 }
 
