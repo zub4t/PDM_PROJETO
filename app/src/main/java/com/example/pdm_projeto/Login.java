de01e652f12a39e5a9cf65329e10e42ced2b2afd
joao <joao.paulo.santiago@gmail.com>
2020-05-18 23:49:51 +0100

    SignIn funcional algumas alterções ao layout do ecrã signIn

--- app/src/main/java/com/example/pdm_projeto/Login.java
+++ app/src/main/java/com/example/pdm_projeto/Login.java
@@ -4,50 +4,72 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.w3c.dom.Text;
 
 
 public class Login extends AppCompatActivity {
-    TextView singup;
-    Button singin;
-    TextView nick;
-    TextView pass;
+
+    ProgressBar progressBar;
+    EditText userEmail;
+    EditText userPass;
+    Button userLogin;
+    TextView userSignUp;
+
+    FirebaseAuth firebaseAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
-        singup = findViewById(R.id.singup);
-        singin  = findViewById(R.id.submit);
-        nick = findViewById(R.id.nickname);
-        pass = findViewById(R.id.password);
 
-        Log.e("teste", String.valueOf(pass.getText().equals("admin")));
-        singin.setOnClickListener(new View.OnClickListener() {
+        progressBar = findViewById(R.id.progressBar2);
+        userEmail = findViewById(R.id.email);
+        userPass = findViewById(R.id.password);
+        userLogin = findViewById(R.id.signin);
+        userSignUp = findViewById(R.id.singup);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        userLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d("teste",nick.getText().toString());
-                Log.d("teste", String.valueOf((nick.getText().toString()).equals("admin")));
-                if((nick.getText().toString()).equals("admin") && (pass.getText().toString()).equals("admin")){
-                    Intent intent;
-                    intent = new Intent(getApplicationContext(), MainActivity.class);
-                    startActivity(intent);
-
-                }
+                progressBar.setVisibility(View.VISIBLE);
+                firebaseAuth.signInWithEmailAndPassword(userEmail.getText().toString(),
+                        userPass.getText().toString())
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                progressBar.setVisibility(View.GONE);
+                                if(task.isSuccessful()) {
+                                    startActivity(new Intent(Login.this, MainActivity.class));
+                                }else{
+                                    Toast.makeText(Login.this, task.getException().getMessage(),
+                                            Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
             }
         });
 
-
-        singup.setOnClickListener(new View.OnClickListener() {
-
+        userSignUp.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent;
-                intent = new Intent(getApplicationContext(), Signup.class);
-                startActivity(intent);
-
+                startActivity(new Intent(Login.this, Signup.class));
             }
         });
     }
