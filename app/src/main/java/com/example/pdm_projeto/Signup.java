18181ae0b4bb860e6b3f8b42e0f282281975b886
joao <joao.paulo.santiago@gmail.com>
2020-05-18 19:35:05 +0100

    signup funcional algumas alterações ao layout do ecrã signup

--- app/src/main/java/com/example/pdm_projeto/Signup.java
+++ app/src/main/java/com/example/pdm_projeto/Signup.java
@@ -2,15 +2,64 @@ package com.example.pdm_projeto;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class Signup extends AppCompatActivity {
+
+    ProgressBar progressBar;
+    EditText email;
+    EditText password;
+    Button signup;
+
+    private FirebaseAuth firebaseAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup);
 
+        email = findViewById(R.id.email_sup);
+        password = findViewById(R.id.password_sup);
+        signup = findViewById(R.id.submit_sup);
+        progressBar = findViewById(R.id.progressBar);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressBar.setVisibility(View.VISIBLE);
+                firebaseAuth.createUserWithEmailAndPassword(email.getText().toString(),
+                        password.getText().toString())
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                progressBar.setVisibility(View.GONE);
+                                if(task.isSuccessful()) {
+                                    Toast.makeText(Signup.this, "Registered Successfully",
+                                            Toast.LENGTH_LONG).show();
+                                    email.setText("");
+                                    password.setText("");
+                                }else{
+                                    Toast.makeText(Signup.this, task.getException().getMessage(),
+                                            Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
+            }
+        });
     }
 }
